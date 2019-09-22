bc69bfe68a0362525e7187506687b4de99396ab3
Justin Chin <justinmichaelchin@gmail.com>
2019-09-22 14:04:07 -0700

    adds ast evaulation

--- parsing.c
+++ parsing.c
@@ -24,6 +24,40 @@ void add_history(char* unused) {}
 #include <readline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if ((strcmp(op, "+") == 0) || strcmp(op, "add") == 0)  { return x + y; }
+  if ((strcmp(op, "-") == 0) || strcmp(op, "sub") == 0)  { return x - y; }
+  if ((strcmp(op, "*") == 0) || strcmp(op, "mult") == 0) { return x + y; }
+  if ((strcmp(op, "/") == 0) || strcmp(op, "div") == 0)  { return x / y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* if tagged as number return directly */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* operator is always second child */
+  char* op = t->children[1]->contents;
+
+  /* store third child in tmp var */
+  long x = eval(t->children[2]);      // notice the recursive call to eval
+
+  /* iterate through rest of children */
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) {
 
   /* create some parsers */
